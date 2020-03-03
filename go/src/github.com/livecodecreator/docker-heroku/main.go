7f92a50eb18ba9fb8c2a55e38ae06a0b5f40db4d
work <satoshi.watabe.sp@gmail.com>
2020-03-03 22:23:15 +0900

    fix

--- go/src/github.com/livecodecreator/docker-heroku/main.go
+++ go/src/github.com/livecodecreator/docker-heroku/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,12 +15,37 @@ func main() {
 	port := os.Getenv("PORT")
 	r := mux.NewRouter()
 	r.HandleFunc("/", DefaultHandler)
+	r.HandleFunc("/slack", SlackHandler)
 	r.HandleFunc("/{wildcard}", DefaultHandler)
 	http.Handle("/", r)
 	err := http.ListenAndServe(fmt.Sprintf(":%s", port), r)
 	if err != nil {
 		log.Printf("err: %v\n", err)
 	}
+}
+
+// SlackChallengeRequest is
+type SlackChallengeRequest struct {
+	Token     string `json:"token"`
+	Challenge string `json:"challenge"`
+	Type      string `json:"type"`
+}
+
+// SlackChallengeResponse is
+type SlackChallengeResponse struct {
+	Challenge string `json:"challenge"`
+}
+
+// SlackHandler is
+func SlackHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	b, _ := ioutil.ReadAll(r.Body)
+	var screq SlackChallengeRequest
+	json.Unmarshal(b, &screq)
+	scres := SlackChallengeResponse{Challenge: screq.Challenge}
+	dat, _ := json.Marshal(scres)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, dat)
 }
 
 // DefaultHandler is
