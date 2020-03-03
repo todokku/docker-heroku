38eb807f791b1ae4faa2b79da6e118d19a8f089e
work <satoshi.watabe.sp@gmail.com>
2020-03-03 22:44:50 +0900

    fix

--- go/src/github.com/livecodecreator/docker-heroku/main.go
+++ go/src/github.com/livecodecreator/docker-heroku/main.go
@@ -14,6 +14,7 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	r := mux.NewRouter()
+	r.Use(ResponseLoggerMiddleware)
 	r.HandleFunc("/", DefaultHandler)
 	r.HandleFunc("/slack", SlackHandler)
 	r.HandleFunc("/{wildcard}", DefaultHandler)
@@ -48,24 +49,31 @@ func SlackHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, string(dat))
 }
 
+// ResponseLoggerMiddleware is
+func ResponseLoggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("request method: %v\n", r.Method)
+		log.Printf("request requestURI: %v\n", r.RequestURI)
+		log.Printf("request url scheme: %v\n", r.URL.Scheme)
+		log.Printf("request url host: %v\n", r.URL.Host)
+		log.Printf("request url path: %v\n", r.URL.Path)
+		log.Printf("request url raw query: %v\n", r.URL.RawQuery)
+
+		for k, v := range r.Header {
+			log.Printf("request header: %v: %v\n", k, v)
+		}
+
+		log.Printf("request body:\n")
+		defer r.Body.Close()
+		b, _ := ioutil.ReadAll(r.Body)
+		log.Println(string(b))
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 // DefaultHandler is
 func DefaultHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("request method: %v\n", r.Method)
-	log.Printf("request requestURI: %v\n", r.RequestURI)
-	log.Printf("request url scheme: %v\n", r.URL.Scheme)
-	log.Printf("request url host: %v\n", r.URL.Host)
-	log.Printf("request url path: %v\n", r.URL.Path)
-	log.Printf("request url raw query: %v\n", r.URL.RawQuery)
-
-	for k, v := range r.Header {
-		log.Printf("request header: %v: %v\n", k, v)
-	}
-
-	log.Printf("request body:\n")
-	defer r.Body.Close()
-	b, _ := ioutil.ReadAll(r.Body)
-	log.Println(string(b))
-
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "OK")
 }
